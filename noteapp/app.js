6c401a4c14ec580d75f9bd98b6e63f882f037be1
ahmedhassan202025 <33345323+ahmedhassan202025@users.noreply.github.com>
2021-03-21 20:02:45 +0200

    module 3 finished

---
+++ noteapp/app.js
@@ -0,0 +1,18 @@
+const fs = require("fs")
+const validator = require('validator');
+const getnotes = require("./notes") 
+const chalk= require('chalk')
+
+console.log(chalk.green.bold("sucess"))
+
+
+
+
+
+
+
+
+// console.log(validator.default.isEmail("ahmedyahoo.com"))
+// getnotes()
+// fs.appendFileSync("note.txt","\n hassona")
+// fs.writeFileSync("note.txt"," My name is ahmed")
---
+++ noteapp/notes.js
@@ -0,0 +1,5 @@
+const  getNotes = function(){
+    
+    console.log("My Notes ..... ")
+}
+module.exports =getNotes
