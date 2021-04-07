e55f7d995e079af4df5e3b5defb7d9b0650fb6dc
ahmedhassan202025 <33345323+ahmedhassan202025@users.noreply.github.com>
2021-04-07 23:53:33 +0200

    add/remove/read/list note commands using yargs

--- noteapp/app.js
+++ noteapp/app.js
@@ -1,18 +1,111 @@
-const fs = require("fs")
-const validator = require('validator');
 const getnotes = require("./notes") 
-const chalk= require('chalk')
-
-console.log(chalk.green.bold("sucess"))
-
+const chalk = require('chalk')
+const yargs = require ('yargs')
 
 
+// // create add command 
+// yargs
+//   .command(
+//     'add',
+//     'adding a note',
+//     {
+//         'title':{
+//             describe:'Note Title',
+//             demandOption:true,
+//             type:'string'
+            
+//         }
+//     },
+//     function (argv) {
+//       console.log('adding a note', argv)
+//     }
+//   )
+//   .help()
+//   .argv
 
+// // create remove command 
+// yargs
+// .command(
+//   'remove',
+//   'removing a note',
+//   function (argv) {
+//     console.log('Removing a note')
+//   }
+// )
+// .help()
+// .argv
 
+// // list command 
+// yargs.command(
+//     'list',
+//     'getting a list',
+//     function(argv)
+//     {
+//         console.log("Listing out all notes" )
+//     }
+// )
+// .help()
+// .argv
 
+// // read command 
+// yargs.command(
+//     'read',
+//     'read a list',
+//     function(argv)
+//     {
+//         console.log("read a list")
+//     }
+// )
+// .help()
+// .argv
 
+// create add command 
+yargs.command({
+    command:'add',
+    describe:'Add a new note',
+    builder:
+    {
+        title:{
+            describe:"Note title",
+            demandOption: true,
+            type:"string"
+        },
+        body:{
+            describe: "Note Body",
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler: function(argv){
+        console.log("Title:" +  argv.title, "body:" + argv.body)
+    }
+})
 
-// console.log(validator.default.isEmail("ahmedyahoo.com"))
-// getnotes()
-// fs.appendFileSync("note.txt","\n hassona")
-// fs.writeFileSync("note.txt"," My name is ahmed")
+// create remove command 
+yargs.command({
+    command:'remove',
+    describe:'remove note',
+    handler: function(argv){
+        console.log("Removing note", argv)
+    }
+})
+
+// create list command 
+yargs.command({
+    command:'list',
+    describe:'Listing out all listes',
+    handler: function(argv){
+        console.log("Listing out all listes", argv)
+    }
+})
+
+// create read command 
+yargs.command({
+    command:'read',
+    describe:'read note',
+    handler: function(argv){
+        console.log("read anote", argv)
+    }
+})
+// this is important to view the commands 
+yargs.parse()
--- noteapp/notes.js
+++ noteapp/notes.js
@@ -1,5 +1,4 @@
 const  getNotes = function(){
-    
     console.log("My Notes ..... ")
 }
 module.exports =getNotes
